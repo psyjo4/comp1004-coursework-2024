45f53280ff117fc9f987249243fb2955d374c830
psyjo4 <psyjo4@nottingham.ac.uk>
2024-04-12 14:25:37 +0800

    Added mutiple search options

--- docs/scripts/supabase.js
+++ docs/scripts/supabase.js
@@ -4,13 +4,14 @@ const supabaseUrl = 'https://eywvespkrnkoeixdzccf.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5d3Zlc3Brcm5rb2VpeGR6Y2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxMDY5MTEsImV4cCI6MjAyNzY4MjkxMX0.xYxfuxOoO7glrfrEdBKFHMir4b9GKj-a6V9zAps7E_o'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function searchByDriverName(name) {
-    const { data, error } = await supabase
+async function searchPeople(nameInput, licenseInput) {
+    let query = supabase
         .from('People')
         .select('*')
-        .ilike('Name', `%${name}%`);
-        // .eq('Name', name);
+        .ilike('Name', `%${nameInput}%`)
+        .ilike('LicenseNumber', `%${licenseInput}%`);
 
+    const { data, error } = await query
     const resultDiv = document.getElementById('results');
 
     if (error) {
@@ -44,8 +45,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.getElementById('search-form');
     searchForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        const name = document.getElementById('name-input').value;
-        searchByDriverName(name);
+        const nameInput = document.getElementById('name-input').value.trim();
+        const licenseInput = document.getElementById('licence-input').value.trim();
+        const inputLength = nameInput.length + licenseInput.length;
+
+        if ((inputLength >= 4) && (nameInput || licenseInput)) {
+            searchPeople(nameInput, licenseInput);
+        } 
+        else {
+            const resultDiv = document.getElementById('results');
+            resultDiv.innerHTML = `<p>Please enter 4 or more characters to search.</p>`;
+            resultDiv.classList.remove('multiple-results');
+        }
     });
 });
 
